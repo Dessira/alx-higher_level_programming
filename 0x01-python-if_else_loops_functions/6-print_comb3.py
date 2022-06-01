473c71a697758fd3a345d0d610239da271e02c9e
Dessira <desirebarine16@gmail.com>
2022-06-01 00:37:43 +0000

    corrections

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for i in range(0, 10):
         if i == 8 and j == 9:
             print("{:d}{:d}".format(i, j))
         elif (i < j):
-            print("{:d}{:d}".format(i, j), end=' ')
+            print("{:d}{:d}".format(i, j), end=', ')
