b5fa4dc2c514d8d4de09eac626d4b887ca39c422
Dessira <desirebarine16@gmail.com>
2022-06-02 20:20:43 +0000

    bug fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,7 @@ if __name__ = "__main__":
     import hidden_4
     contents = dir(hidden_4)
     size = len(contents)
+    dod = "__"
     for i in range(0, size):
-        if '__' not in contents[i]:
+        if dod not in contents[i]:
             print(contents[i])
