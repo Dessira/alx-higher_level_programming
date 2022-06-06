49d0783c91fc257f6ed3d2b7c6477e612ca9d5c3
Dessira <desirebarine16@gmail.com>
2022-06-06 14:25:05 +0000

    done!!! for now

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,5 @@ def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         return
     skip = len(my_list)
-    for i in range(skip -1, -1, -1):
+    for i in range(skip - 1, -1, -1):
         print("{:d}".format(my_list[i]))
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0 or my_list is None:
         return None
-    high = 0
+    high = my_list[0]
     for index in my_list:
         if index > high:
             high = index
