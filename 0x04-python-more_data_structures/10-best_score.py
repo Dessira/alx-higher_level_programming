218ad387e6d14995f6f079f349f08b05fe9298b9
Dessira <desirebarine16@gmail.com>
2022-06-09 23:06:04 +0000

    minor fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     val = list(a_dictionary.keys())[0]
     high = a_dictionary[val]
-    for i in a_dictionary:
-        if a_dictionary[i] > high:
-            high = a_dictionary[i]
+    for i, j in a_dictionary.items():
+        if j > high:
+            high = j
             k = i
     return k
