22a823775b3cd928169c3b9e3b34941ab69468dd
Dessira <desirebarine16@gmail.com>
2022-06-20 15:05:19 +0000

    updates

--- 100-weight_average.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def weight_average(my_list=[]):
-    if my_list == None:
-        return 0
-    
--- 2-uniq_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def uniq_add(my_list=[]):
-    new_list = list(set(my_list))
-    sum = new_list[0]
-    for i in range(1, len(new_list)):
-        sum += new_list[i]
-        print(sum)
-    return sum
