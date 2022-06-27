164b53171a85100a4bd346e16b05b0549e01785f
Dessira <desirebarine16@gmail.com>
2022-06-27 14:11:26 +0000

    tasks

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+    @property
+    def size(self):
+        return(self.__size)
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    @property
+    def position(self):
+        return(self.__position)
+    @size.setter
+    def position(self, position):
+        if position[0] < 0 | position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+    def area(self):
+        return self.__size * self.__size
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for k in range(self.__position[0]):
+                    print(" ", end='')
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
