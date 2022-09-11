9a43167dd0cf32e2dd8ebe45fd47a9f1479571a6
Dessira <desirebarine16@gmail.com>
2022-09-11 14:52:20 -0700

    bug fix2

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- Creates the database hbtn_0d_usa and the table cities 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa: 
-CREATE TABLE IF NOT EXISTS htbn_0d_usa.cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	state_id int NOT NULL,
 	name VARCHAR(256) NOT  NULL,
