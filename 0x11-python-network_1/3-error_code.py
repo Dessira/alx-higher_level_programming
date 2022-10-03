158bdb3ddb83da48332995cecd24171acb76cc8a
desire barine <desirebarine16@gmail.com>
2022-10-03 07:25:23 +0000

    bug fix

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Script that takes in a URL, sends a request to the URL
-and displays the body of the response
 """
 
 import sys
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,6 +8,6 @@ import requests
 
 if __name__ == "__main__":
 
-    value = {'email': sys.argv[2]}
-    r = requests.post(sys.argv[1], params=value)
+    value = {"email": sys.argv[2]}
+    r = requests.post(sys.argv[1], data=value)
     print(r.text)
