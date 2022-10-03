ccd7afad22f310696830a7162cb415d0db1756e2
desire barine <desirebarine16@gmail.com>
2022-10-03 12:29:25 +0000

    fixed task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
         with urllib.request.urlopen(req) as f:
             print(f.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
-        print("Error code: ", e.code)
+        print("Error code:", e.code)
