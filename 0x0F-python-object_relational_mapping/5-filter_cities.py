94b78da40fd8319b709fd27525ddd9582c60e8b3
Desire Barine <desirebarine16@gmail.com>
2022-12-13 08:28:32 +0100

    fixes

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,7 +6,7 @@
     database name: arg[3]
 """
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     db_con = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
